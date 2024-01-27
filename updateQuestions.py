6618b756e202e07517f1df2c7b2b7f716a4800f6
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-27 21:24:29 +0530

    Mentify Quiz Updater

--- updateQuestions.py
+++ updateQuestions.py
@@ -12,11 +12,24 @@ genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel('gemini-pro')
 
-with open("Anger.d.ts", "r") as f:
-    readData = f.read()
+# get all the *.d.ts files in the directory
+for filename in os.listdir('./'):
+    if filename.endswith(".d.ts"):
+        with open(filename, 'r') as file:
+            readData = file.read()
+            file.close()
+            prompt = prompt = f"update the questions with new questions in the beside data and give me the output in json format => {readData}"
+            response = model.generate_content(prompt)
+            print(response.text + "\n")
+            continue
+    else:
+        continue
+    
+# with open("Anger.d.ts", "r") as f:
+#     readData = f.read()
 
-prompt = prompt = f"update the questions in the beside data and give me the output in json format => {readData}"
+# prompt = prompt = f"update the questions with new questions in the beside data and give me the output in json format => {readData}"
 
-response = model.generate_content(prompt)
+# response = model.generate_content(prompt)
 
-print(response.text)
+# print(response.text)
